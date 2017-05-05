c42071c684f2c4160ec09416ed30f3bc8880970e
William Lu <william.a.lu@gmail.com>
2017-05-04 20:54:44 -0400

    removed old plot, updated plotting script

--- rail_to_rail_opamp/gm_graph.py
+++ rail_to_rail_opamp/gm_graph.py
@@ -1,9 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import seaborn as sns
 import csv
 
 from pprint import pprint
+
+# set matplotlib fonts to CMU Serif
+mpl.rc('font',**{'family':'serif','serif':['CMU Serif']})
+mpl.rc('text', usetex=True)
+
 
 def getData(FILENAME):
     data = []
@@ -51,7 +57,7 @@ if __name__ == "__main__":
         gm.append(makeFit(x, y))
     
     plt.plot(Vcm, gm)
-    plt.xlabel('$V_{cm}$ (V)', fontsize=14)
-    plt.ylabel('$g_m$', fontsize=14)
-    plt.title('$g_m$ vs. $V_{cm}$ of a Rail to Rail Diff Amp', fontsize=16)
+    plt.xlabel('$\mathrm{V_{cm}}$ (V)', fontsize=14)
+    plt.ylabel('$\mathrm{g_m}$ (S)', fontsize=14)
+    plt.title('$\mathrm{g_m}$ vs. $\mathrm{V_{cm}}$ of a Rail to Rail Diff Amp', fontsize=16)
     plt.show()
